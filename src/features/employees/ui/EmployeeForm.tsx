29571990f1d9ef6c21be78f5e5ff168cac35c655
IslamDevelop <islam.magomaev@mail.ru>
2025-03-07 21:57:23 +0300

    add mask and validation

--- src/features/employees/ui/EmployeeForm.tsx
+++ src/features/employees/ui/EmployeeForm.tsx
@@ -1,7 +1,7 @@
 import { useForm, Controller } from "react-hook-form";
 import { Employee } from "../../../entities/employee/types";
-
 import styles from "./EmployeeForm.module.scss";
+import { InputMask } from "@react-input/mask";
 
 interface EmployeeFormProps {
   employee?: Employee;
@@ -9,7 +9,7 @@ interface EmployeeFormProps {
 }
 
 export const EmployeeForm = ({ employee, onSubmit }: EmployeeFormProps) => {
-  const { control, handleSubmit } = useForm<Employee>({
+  const { control, handleSubmit, formState: { errors } } = useForm<Employee>({
     defaultValues: employee || {
       id: 0,
       name: "",
@@ -36,19 +36,41 @@ export const EmployeeForm = ({ employee, onSubmit }: EmployeeFormProps) => {
         <Controller
           name="phone"
           control={control}
+          rules={{
+            required: "Телефон обязателен",
+            pattern: {
+              value: /^\+7\s\(\d{3}\)\s\d{3}-\d{4}$/, 
+              message: "Введите в формате +7(999) 999-9999", 
+            },
+          }}
           render={({ field }) => (
-            <input {...field} placeholder="Телефон" required />
+            <InputMask mask="+7 (___) ___-____" replacement={{ _: /\d/ }} placeholder="+7(999) 999-9999" {...field} />
           )}
         />
+        {errors.phone && (
+              <p style={{ color: "red" }}>{errors.phone.message}</p>
+            )}
+         
 
         <label>Дата рождения</label>
         <Controller
           name="birthday"
           control={control}
+          rules={{
+            required: "Введите дату рождения",
+            pattern: {
+              value: /^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[0-2])\.\d{4}$/,
+              message: "Некорректный формат даты (дд.мм.гггг)",
+            },
+          }}
           render={({ field }) => (
-            <input {...field} placeholder="дд.мм.гггг" required />
+            <InputMask type="data" mask="__.__.____" {...field} replacement={{ _: /\d/ }} placeholder="дд.мм.гггг"  />
+            
           )}
         />
+        {errors.birthday && (
+              <p style={{ color: "red" }}>{errors.birthday.message}</p>
+            )}
 
         <label>Должность</label>
         <Controller
